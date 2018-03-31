2dd2e19cc67e97ba8583ed0181ba0d0cbb647317
Elliot Anderson <elliot@saferespondr.com>
2018-03-31 04:50:51 -0400

    Adding base code with loop

---
+++ MetaCommandResult.h
@@ -0,0 +1,10 @@
+#ifndef METACOMMANDRESULT
+#define METACOMMANDRESULT
+
+enum MetaCommandResult_t {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+};
+typedef enum MetaCommandResult_t MetaCommandResult;
+
+#endif
---
+++ PrepareResult.h
@@ -0,0 +1,7 @@
+#ifndef PREPARE_RESULT
+#define PREPARE_RESULT
+
+enum PrepareResult_t { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT };
+typedef enum PrepareResult_t PrepareResult;
+
+#endif
